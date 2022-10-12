0a5044b01d4fc747a6ed7f65ce8cba7eaa889d3a
David Ansermot <dansermot@openwt.com>
2022-10-12 09:04:48 +0200

    First files import

---
+++ MobileProfileExtract/Classes/Controllers/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  MobileProfileExtract
+//
+//  Created by David Ansermot on 12.10.22.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
